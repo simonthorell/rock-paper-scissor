b9ef3b992636632f939ab86223427d3b31701138
Simon Thorell <kontakt@simonthorell.se>
2023-10-06 11:48:03 +0200

    Created Game Logic that will return winner or aadvise if round is tie.

---
+++ src/GameLogic.java
@@ -0,0 +1,31 @@
+public class GameLogic {
+    private int player1;
+    private int player2;
+
+    public GameLogic(int player1, int player2) {
+        this.player1 = player1;
+        this.player2 = player2;
+    }
+
+    public int getWinner() {
+        // Return 0 = Tie, Return 1 = player1 wins, Return 2 = player2 wins
+        if (player1 == player2) {
+            return 0;
+        } else if (player1 == 1 && player2 == 2) {
+            return 2;
+        } else if (player1 == 1 && player2 == 3) {
+            return 1;
+        } else if (player1 == 2 && player2 == 1) {
+            return 1;
+        } else if (player1 == 2 && player2 == 3) {
+            return 2;
+        } else if (player1 == 3 && player2 == 1) {
+            return 2;
+        } else if (player1 == 3 && player2 == 2) {
+            return 1;
+        } else {
+            return 0;
+        }
+    }
+
+}
