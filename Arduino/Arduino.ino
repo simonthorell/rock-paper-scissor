72885d308b4af7d7fc48606267e1d43911acc42f
Emil Kool <EmilhKool@gmail.com>
2023-10-07 12:18:20 +0200

    Added functionality to wait for playerID

--- Arduino/Arduino.ino
+++ Arduino/Arduino.ino
@@ -23,16 +23,22 @@ const char keys[4][4] = {
     { '1', '2', '3', 'A'},
     { '4', '5', '6', 'B'},
     { '7', '8', '9', 'C'},
-    { '*', '0', '#', 'D'}
-};
+    { '*', '0', '#', 'D'}};
 byte rowPins[4] = {11, 10, 9, 8};
 byte colPins[4] = {7, 6, 5, 4};
+
+uint8_t playerID = 0;
+uint8_t waiting = 0;
+char pressedKey;
 
 Keypad myKeypad = Keypad(makeKeymap(keys), rowPins, colPins, 4, 4);
 LiquidCrystal_I2C lcd(0, 0, 0);
 
-//forward declarations
-void getChoice(char c);
+//forward declarations, do you even need these for the compiler here?
+void getKeypadPress(char c);
+void waitForID();
+void printSelection(char* str);
+
 
 void setup(){
     lcd = aLCD::startLCD();
@@ -40,16 +46,38 @@ void setup(){
 }
 
 void loop(){
-    char keyPressed = myKeypad.getKey();
-    if(keyPressed)
-        getChoice(keyPressed);
+    if(playerID == 0 ? false : true){
+        pressedKey = myKeypad.getKey();
+        if(pressedKey)
+            getKeypadPress(pressedKey);
+    }
+    else{
+        waitForID();
+    }
 }
 
 /* 
-Preparing a interrupt for displaying if you win or lose later
+Get stuck in loop while we wait for a playerID over USB, we escape the loop by the interrupt function
+*/
+void waitForID(){
+    Serial.println("NO PLAYER ID");
+    lcd.clear();
+    lcd.print("Connecting");
+    for(int i = 0; i <= waiting % 3; i++)
+        lcd.print(".");
+    delay(500);
+    waiting++;
+}
+
+/* 
+Interrupt for serialEvents to read data and process it before jumping back to the previous program state
+Char byte values
+87 = W
+76 = L
  */
 void serialEvent(){
     char inChar;
+    Serial.println("Serial event interrupt");
 
     if(Serial.available()){
         inChar = Serial.read();
@@ -65,6 +93,14 @@ void serialEvent(){
             lcd.clear();
             lcd.print("You lost!");
         }
+        else{
+            lcd.clear();
+            playerID = inChar;
+            lcd.print("PlayerID: ");
+            lcd.print(playerID);
+            lcd.setCursor(0, 1);
+            lcd.print("READY TO PLAY!");
+        }
     }
 }
 
@@ -74,28 +110,41 @@ Char byte values
 2 = 50
 3 = 51
  */
-void getChoice(char c){
-    lcd.clear();
-    switch(c)
-    {
+void getKeypadPress(char c){
+    //lcd.clear();
+    switch(c){
         case 49:
             Serial.println(1);
-            lcd.print("Rock");
+            printSelection("Rock");
         break;
         
         case 50:
             Serial.println(2);
-            lcd.print("Paper");
+            printSelection("Paper");
         break;
 
         case 51:
             Serial.println(3);
-            lcd.print("Scissor");
+            printSelection("Scissor");
         break;
 
         default:
-            Serial.println(4);
+            lcd.clear();
             lcd.print("Press 1, 2 or 3!");
+            lcd.setCursor(0, 1);
+            lcd.print(c);
         break;
     }
+}
+
+void printSelection(char* str)
+{
+    lcd.clear();
+    lcd.print("Selected: ");
+/*     int i = 0;
+    while(*(str + i) != 0){
+        lcd.print(*(str + i));
+        i++;
+    }  */      
+    lcd.print(str);
 }
