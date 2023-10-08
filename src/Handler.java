53f37cc1ab934f45f596b80053c59de4645b70ff
Simon Thorell <kontakt@simonthorell.se>
2023-10-09 01:46:41 +0200

    Created boiler plate for tournament tree logic

--- src/Handler.java
+++ src/Handler.java
@@ -1,7 +1,7 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
 import org.eclipse.paho.client.mqttv3.MqttException;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Handler {
     private boolean runGame = true;
@@ -9,66 +9,74 @@ public class Handler {
     private List<PlayerStatus> players;
 
     public Handler() throws MqttException, InterruptedException {
-        while(runGame) {
-            // run gameMenuGUIjavaClass();
-            // this GUI class should return a boolean value = false when pushing a button to exit the game.     
-        
-            // FOR TESTING OF ARDUINO MULTIPLAYER
+        while (runGame) {
+            // Add GUI interactions and other pre-game setup here...
+
             multiPlayer();
             Thread.sleep(5000); // SLEEP FOR 5 SECONDS BEFORE RUNNING GAME AGAIN
         }
     }
 
+    // Additional methods for single player or other game modes...
     public void singlePlayer() {
-        // run singlePlayerGUIjavaClass();
-        // create one new instance of PlayerStatus and one new instance of ComputerStatus.
-        // CountPlayerID should be incremented for each player in order to have unique object ID's. 
+        // Implement single player game mode
     }
 
     public void multiPlayer() throws MqttException, InterruptedException {
-        // run multiPlayerGUIjavaClass();
-        final int MAX_PLAYERS = 2;     // Should be set to higher amount for tournament option. 
+        // [Adjustable] MAX_PLAYERS could be dynamic based on a GUI interaction or game setup stage
+        final int MAX_PLAYERS = 2; 
         String displayMessage = "Push any button to play!";
         String[] countDownMsg = {"3", "2", "1", "Rock, Paper, Scissors!"};
-        boolean waitForPlayers = true; // Should be set to false from GUI when all players are ready to play.
+        boolean waitForPlayers = true;
         players = new ArrayList<>();
 
-        while (waitForPlayers == true && countPlayerID < MAX_PLAYERS) {
+        while (waitForPlayers && countPlayerID < MAX_PLAYERS) {
             countPlayerID++;
             PlayerStatus currentPlayer = new PlayerStatus(countPlayerID, true);
             players.add(currentPlayer);
-            currentPlayer.mqttPlayer().askToPlay(displayMessage, countPlayerID); // Send msg as object with msg and next available player ID/topic
-            currentPlayer.mqttPlayer().getMove(); // Get move from player
+            currentPlayer.mqttPlayer().askToPlay(displayMessage, countPlayerID);
+            currentPlayer.mqttPlayer().getMove();
         }
 
-        // TOURNAMENT TREE GAME LOGIC HERE
+        // TOURNAMENT LOGIC HERE...
         TournamentTree tournamentTree = new TournamentTree(players);
-        List<PlayerStatus> next2Players = Arrays.asList(tournamentTree.nextGame()); // Play next game in tournament tree
 
-        PlayerStatus player1 = next2Players.get(0);
-        PlayerStatus player2 = next2Players.get(1);
+        while (true) {
+            TournamentTree.Node nextMatch = tournamentTree.nextGame();
 
-        MqttPlayer.countDownAndThrow(countDownMsg);
+            if (nextMatch == null) {
+                System.out.println("Tournament over!");
+                break;
+            }
 
-        player1.setPlayerMove(player1.mqttPlayer().getMove());
-        player2.setPlayerMove(player2.mqttPlayer().getMove());
+            PlayerStatus player1 = nextMatch.player1;
+            PlayerStatus player2 = nextMatch.player2;
 
-        GameLogic gameLogic = new GameLogic(player1.getPlayerMove(), player2.getPlayerMove());
-        int winner = gameLogic.getWinner();
+            MqttPlayer.countDownAndThrow(countDownMsg);
 
-        if (winner == 0) {
-            MqttPlayer.displayGameResult("Tie!");
-        } else if (winner == 1) {
-            MqttPlayer.displayGameResult("Player 1 won!");
-        } else if (winner == 2) {
-            MqttPlayer.displayGameResult("Player 2 won!");
-        } else {
-            MqttPlayer.displayGameResult("Error!");
+            player1.setPlayerMove(player1.mqttPlayer().getMove());
+            player2.setPlayerMove(player2.mqttPlayer().getMove());
+
+            GameLogic gameLogic = new GameLogic(player1.getPlayerMove(), player2.getPlayerMove());
+            int winner = gameLogic.getWinner();
+
+            PlayerStatus winningPlayer = null;
+            if (winner == 1) {
+                MqttPlayer.displayGameResult("Player 1 won!");
+                winningPlayer = player1;
+            } else if (winner == 2) {
+                MqttPlayer.displayGameResult("Player 2 won!");
+                winningPlayer = player2;
+            } else {
+                MqttPlayer.displayGameResult("Tie!");
+                continue;
+            }
+
+            tournamentTree.reportMatchResult(nextMatch, winningPlayer);
         }
 
         for (PlayerStatus currentPlayer : players) {
             currentPlayer.mqttPlayer().disconnect();
         }
     }
-
 }
--- src/TournamentTree.java
+++ src/TournamentTree.java
@@ -1,18 +1,63 @@
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class TournamentTree {
-    private List<PlayerStatus> players;
+    private Node root;
+    private Queue<Node> nextMatches = new LinkedList<>();
 
     public TournamentTree(List<PlayerStatus> players) {
-        this.players = players;
+        // Assumed players.size() is a power of 2, error checking should be added for robustness
+        Queue<Node> matchesQueue = new LinkedList<>();
+        for (int i = 0; i < players.size(); i += 2) {
+            matchesQueue.add(new Node(players.get(i), players.get(i + 1)));
+        }
+
+        while (matchesQueue.size() > 1) {
+            matchesQueue.add(new Node(matchesQueue.poll(), matchesQueue.poll()));
+        }
+
+        this.root = matchesQueue.poll();
+        this.nextMatches.add(root); // Add the root node as the initial match
     }
 
-    public PlayerStatus[] nextGame() {
-        // THIS LOGICS SHOULD ONLY RUN 2 PLAYERS AGAINST EACH OTHER AT A TIME - BELOW IS ONLY FOR TESTING USING FIRST 2 PLAYERS IN LIST.
-        PlayerStatus player1 = players.get(0);
-        PlayerStatus player2 = players.get(1);
-        return new PlayerStatus[] { player1, player2 };
+    public Node nextGame() {
+        return nextMatches.poll();
     }
 
+    public void reportMatchResult(Node matchNode, PlayerStatus winner) {
+        matchNode.winner = winner;
+
+        if (matchNode.parent != null) {
+            // If matchNode's sibling has a winner, and the parent hasn't, add parent to nextMatches
+            Node sibling = (matchNode == matchNode.parent.leftChild) ?
+                    matchNode.parent.rightChild : matchNode.parent.leftChild;
+            if (sibling.winner != null && matchNode.parent.winner == null) {
+                nextMatches.add(matchNode.parent);
+            }
+        }
+    }
+
+    public static class Node {
+        PlayerStatus player1;
+        PlayerStatus player2;
+        PlayerStatus winner = null;
+        Node parent;
+        Node leftChild;
+        Node rightChild;
+
+        // Node for a match with actual players
+        public Node(PlayerStatus player1, PlayerStatus player2) {
+            this.player1 = player1;
+            this.player2 = player2;
+        }
+
+        // Node for a match with child matches
+        public Node(Node leftChild, Node rightChild) {
+            this.leftChild = leftChild;
+            this.rightChild = rightChild;
+            leftChild.parent = this;
+            rightChild.parent = this;
+        }
+    }
 }
-
