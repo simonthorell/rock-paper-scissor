98dae7b2cbaaad7ec680d49e90d78938aa17951a
Simon Thorell <kontakt@simonthorell.se>
2023-10-08 21:43:47 +0200

    Some small changes to scriptPlayer

--- scriptPlayer/MQTT_player.py
+++ scriptPlayer/MQTT_player.py
@@ -2,6 +2,7 @@ import paho.mqtt.client as mqtt
 import json
 import ssl
 import threading
+from time import sleep
 
 BROKER_URL = "1c87c890092b4b9aaa4e1ca5a02dfc9e.s1.eu.hivemq.cloud"
 PORT = 8883
@@ -9,34 +10,45 @@ USERNAME = "W-bot"
 PASSWORD = "W-bot123!"
 TOPIC_PREFIX = "sten-sax-pase/"
 
+wait = True
 playerID = None
 latest_playerID = None
-rockPaperScissor = None
-countDown = 3
+askForInput = False
+counter = 3
+
+def waiting(wait_message):
+    global wait
+    while wait:
+        print(f"\r{wait_message}", end="", flush=True)
+        for _ in range(5):
+            sleep(0.5)
+            if not wait:
+                break
+            print(".", end="", flush=True)
 
 def on_connect(client, userdata, flags, rc):    
-    print(f"Connected with result code {str(rc)}")
+    print(f"Connected successfully to Sten Sax Påse! (result code {str(rc)})")
     client.subscribe(f"{TOPIC_PREFIX}message")
 
 def on_message(client, userdata, message):
-    global latest_playerID
-    global countDown
+    global wait, playerID, latest_playerID, askForInput, counter
+    wait = False
 
     payload_str = message.payload.decode('utf-8')
     payload_json = json.loads(payload_str)
     latest_playerID = payload_json.get('playerID')
 
     if playerID is None:
-        print(f"\nNew playerID received: {latest_playerID}")
-        # print("Enter a command: ", end="", flush=True)
+        print(f"\n{payload_json.get('message')}")
+        askForInput = True
     elif payload_json.get('playerID') is None:
-        print(payload_json.get('message'))
-        countDown -= 1
+        print(f"{payload_json.get('message')}")
+        counter -= 1
+        if counter < 0:
+            askForInput = True
     else:
-        print("Waiting for new players...")
-
-    if  playerID is None or countDown < 0:
-        print("Enter a command: ", end="", flush=True)
+        print("Waiting for new players to join...")
+        askForInput = False
 
 def mqtt_listener(client):
     client.on_connect = on_connect
@@ -46,28 +58,20 @@ def mqtt_listener(client):
     client.loop_forever()
 
 def user_input(client):
-    global latest_playerID
-    global playerID
-    global rockPaperScissor
-
-    while playerID is None:
-        user_command = input("Enter a command: ")
-
-        if user_command.lower() == 'exit':
-            break
-        elif latest_playerID is not None:
-            client.publish(f"{TOPIC_PREFIX}player{latest_playerID}", json.dumps({"message": user_command}))
-            playerID = latest_playerID
-            print(f"Executing '{user_command}' for playerID: {playerID}")
-        else:
-            print("No playerID received yet")
+    global latest_playerID, playerID, askForInput
     
-    while rockPaperScissor is None:
-        user_command = input("Enter a command: ")
-        client.publish(f"{TOPIC_PREFIX}player{playerID}", json.dumps({"message": user_command}))
-        print(f"Executing '{user_command}' for playerID: {playerID}")
-        rockPaperScissor = user_command
+    while True:
+        if askForInput is True:
+            user_command = input("Enter a command: ")
+            
+            if playerID is None:
+                playerID = latest_playerID
+                print(f"Welcome Player {playerID}!")
 
+            client.publish(f"{TOPIC_PREFIX}player{playerID}", json.dumps({"message": user_command}))
+            askForInput = False
+
+        sleep(1)
 
 if __name__ == "__main__":
     client = mqtt.Client()
@@ -77,6 +81,8 @@ if __name__ == "__main__":
     user_input_thread = threading.Thread(target=user_input, args=(client,))
 
     listener_thread.start()
+    sleep(3) # Give some time for the connection to establish
+    waiting("Waiting for host to start game")
     user_input_thread.start()
 
     listener_thread.join()
