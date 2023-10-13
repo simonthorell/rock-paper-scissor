31416c2163e8e84b0e12c98e2f0d04749abf096c
petterssonb <erikpettersson02@gmail.com>
2023-10-13 11:06:42 +0200

    Modified

--- src/HighScore.java
+++ src/HighScore.java
@@ -1,11 +1,27 @@
+import java.util.ArrayList;
 import java.util.List;
 
 public class HighScore {
     
-    public static void displayRankOrder(List<PlayerStatus> rankedPlayers){
+    public static List displayRankOrder(List<PlayerStatus> rankedPlayers){
+        
         int rank = 1;
+        List<String> ranked = new ArrayList<>();
+        
+        
         for(PlayerStatus player : rankedPlayers){
+            ranked.add ("Rank " + rank + ": " + player.getPlayerID() + " - Score: " + player.getScore());
             System.out.println("Rank " + rank + ": " + player.getPlayerID() + " - Score: " + player.getScore());
         }
+
+
+        return ranked;
+
+
+
+
+        
+
+        
     }
 }
