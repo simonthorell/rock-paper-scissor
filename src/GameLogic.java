39f598cd1b4c67f92f7264f1506f6860063d2264
NicoleNilsson <nicole-nilsson@outlook.com>
2023-10-13 11:58:35 +0200

    changed conditions in gamelogic

--- src/GameLogic.java
+++ src/GameLogic.java
@@ -11,17 +11,17 @@ public class GameLogic {
         // Return 0 = Tie, Return 1 = player1 wins, Return 2 = player2 wins
         if (player1 == player2) {
             return 0;
+        } else if (player1 == 0 && player2 == 1) {
+            return 2;
+        } else if (player1 == 0 && player2 == 2) {
+            return 1;
+        } else if (player1 == 1 && player2 == 0) {
+            return 1;
         } else if (player1 == 1 && player2 == 2) {
             return 2;
-        } else if (player1 == 1 && player2 == 3) {
-            return 1;
+        } else if (player1 == 2 && player2 == 0) {
+            return 2;
         } else if (player1 == 2 && player2 == 1) {
-            return 1;
-        } else if (player1 == 2 && player2 == 3) {
-            return 2;
-        } else if (player1 == 3 && player2 == 1) {
-            return 2;
-        } else if (player1 == 3 && player2 == 2) {
             return 1;
         } else {
             return 0;
