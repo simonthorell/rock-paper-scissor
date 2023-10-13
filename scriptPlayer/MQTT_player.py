64aa7d28383a22412b651b828e9f47672e903780
Simon Thorell <kontakt@simonthorell.se>
2023-10-13 10:38:59 +0200

    Reverted MQTT python script player as Arduino script now in separate file

--- scriptPlayer/MQTT_player.py
+++ scriptPlayer/MQTT_player.py
@@ -3,8 +3,6 @@ import json
 import ssl
 import threading
 from time import sleep
-import serial
-import sys
 
 BROKER_URL = "1c87c890092b4b9aaa4e1ca5a02dfc9e.s1.eu.hivemq.cloud"
 PORT = 8883
@@ -75,44 +73,6 @@ def user_input(client):
 
             client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"message": user_command}))
             ask_for_input = False
-            
-def arduinoUSBDecode(incoming_byte, client, serial_bus):
-    global player_id, latest_player_id
-    match incoming_byte:
-        
-        case 49: #0b00110001 #Ascii 1
-            print("Rock")
-            client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"message": "1"}))
-            
-        case 50: #0b00110010 #Ascii 2
-            print("Paper")
-            client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"message": "2"}))
-            
-        case 52: #0b00110100 #Ascii 4
-            print("Scissor")
-            client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"message": "3"}))
-            
-        case 68: #0b01000100: #Upper case D ascii
-            print("requesting player ID")
-            #player_id = None #temp shit
-            #latest_player_id = 2 #temp shit
-            #just copied the structure from user_input()
-            if player_id == None:
-                player_id = latest_player_id
-                print("playerID: " + str(player_id))
-                serial_bus.write(player_id.to_bytes(1, "big")) #Convert to 1 byte so arduino can read
-                client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"message": "test"}))
-                            
-def serial_listener(client):
-    baud_rate = 9600
-    usb_port = "COM3"
-    serial_bus = serial.Serial(usb_port, baud_rate, timeout = 1)
-    
-    while True:
-        if serial_bus.in_waiting > 0:
-            message = serial_bus.readline()
-            for byte in message:
-                arduinoUSBDecode(byte, client, serial_bus)
 
 if __name__ == "__main__":
     client = mqtt.Client()
@@ -120,23 +80,11 @@ if __name__ == "__main__":
 
     listener_thread = threading.Thread(target=mqtt_listener, args=(client,))
     user_input_thread = threading.Thread(target=user_input, args=(client,))
-    serial_listen_thread = threading.Thread(target = serial_listener, args=(client,))
 
     listener_thread.start()
     sleep(2.2) # Give some time for the connection to establish
     waiting("Waiting for host to start game")
-    
-    arduino = False
-    for i in sys.argv:
-        if(i == "-A"):
-            print("Arduino Mode")
-            serial_listen_thread.start()
-            serial_listen_thread.join()
-            arduino = True
-            
-    if not arduino:
-        print("Default mode")
-        user_input_thread.start()
-        user_input_thread.join()
+    user_input_thread.start()
 
     listener_thread.join()
+    user_input_thread.join()
