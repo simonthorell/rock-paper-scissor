f13c377e7a1909060243d5b134e4ea1bdb94df30
Emil Kool <EmilhKool@gmail.com>
2023-10-13 16:56:33 +0200

    Removed redunant code, moving to existing system

--- scriptPlayer/Arduino_player.py
+++ scriptPlayer/Arduino_player.py
@@ -14,6 +14,7 @@ usb_port = "COM3"
 
 player_id = -1
 result = 5
+displayMessage = ""
 
 BROKER_URL = "1c87c890092b4b9aaa4e1ca5a02dfc9e.s1.eu.hivemq.cloud"
 PORT = 8883
@@ -26,27 +27,32 @@ def on_connect(client, userdata, flags, rc):
     print(f"\033[1mConnected successfully to Sten Sax Påse!\033[0m \033[3m(Connection result code {str(rc)})\033[0m")
 
 def on_message(client, userdata, message):
-    global player_id, result
+    global player_id, result, displayMessage
     
     payload_str = message.payload.decode("utf-8")       #Decode the incoming message as a utf-8 string
     payload_json = json.loads(payload_str)              #make a json object of it
     print(payload_json)                                 #debug print for my sanity
-    latest_player_id = payload_json.get("playerID")     #Get playerID
-    incoming_MAC = payload_json.get("MAC")              #Get MAC
-    incoming_result = payload_json.get("result")        #Get result
+    incoming_message = payload_json.get("message")      #Get the message
     
-    if(incoming_MAC):                                                               #If we had a MAC key in the incoming message
-        if(uuid.getnode() == incoming_MAC & player_id != -1):                       #Check if its our MAC adress and that we havent already binded to a playerID
-            print("uuid match")                                                     #Debug text
-            player_id = latest_player_id                                            #Bind our playerID
-                                                                                    
-            payload_dict = {"playerID" : player_id, "MAC" : int(uuid.getnode())}    #The info we want to send
-            client.publish(f"{TOPIC_PREFIX}message", json.dumps(payload_dict))      #Confirmation message with playerID + mac so backend knows which one it picked
-            client.subscribe(f"{TOPIC_PREFIX}player{player_id}")                    #Change the topic we are subscribed to into the playerID specific one
-    
-    if(incoming_result):                                                            #If we have a result key in the json
-        result = payload_json.get("result")                                         #Set the global result variable to it, will be used by the other thread
+    if(incoming_message):                               #If we have a message key in the json
+        msgPayload = payload_json.get("message")       
         print(result)
+        #TODO Decode result for different actions
+        
+        if (msgPayload == "Push any button to play!"):
+            player_id = payload_json.get("playerID")
+            
+        #TODO fix med so it actually works
+        elif (msgPayload == "player x won"):
+            result = 1 
+            
+        elif (msgPayload == "3"):
+            result = 3
+            
+        elif (msgPayload == "write"):
+            displayMessage = payload_json.get("write")
+            result = 2
+                
     
 def mqtt_listener(client): #Setup the mqtt client
     client.on_connect = on_connect
@@ -63,19 +69,17 @@ def arduinoUSBDecode(incoming_byte, client, serial_bus):
     match incoming_byte: #Runs for each byte thats sent
         
         case 49: #ASCII 1 rock
-            client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"move" : "1"}))
+            client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"message" : "0"}))
             
         case 50: #ASCII 2 paper
-            client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"move" : "2"}))
+            client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"message" : "1"}))
             
         case 52: #ASCII 4 scissor
-            client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"move" : "3"}))
+            client.publish(f"{TOPIC_PREFIX}player{player_id}", json.dumps({"message" : "2"}))
 
         case 68: #ASCII D request ID
             print("Waiting for ID")
-            if(player_id == -1): #The default value
-                request_player_id(client)
-            else: #We have gotten a player ID so send to arduino
+            if(player_id != -1):
                 serial_bus.write(player_id.to_bytes(1, "big"))
                 #Big is big endian
                 
@@ -92,11 +96,12 @@ def arduinoWriteToScreen(serial_bus, message):
     serial_bus.write(b'\x00') #Null-termination
 
 def serial_listen(client):
-    global result
+    global result, displayMessage
     
     #Try to open the serial bus
     try:
         serial_bus = serial.Serial(usb_port, baud_rate, timeout = 1)
+        #serial_bus.write(b'\x05') #Acknowledge to raise flag in arduino that connection is made
         
     #If we cant open the serial bus terminate the entire python script
     except:
@@ -112,21 +117,25 @@ def serial_listen(client):
                 
         elif result != 5: #Just chose a arbitrary value, 5 means we have intercepted a W/L/T over MQTT
             match result: #Simple switch to get the actual value
-                case 1:
-                    serial_bus.write(b'W') #Write win to arduino
+                case 1: #Write win to arduino
+                    serial_bus.write(b'W') 
                     result = 5
                     
-                case 0:
-                    serial_bus.write(b'T') #Write tie to arduino
+                case 0: #Write tie to arduino
+                    serial_bus.write(b'T') 
                     result = 5
                     
-                case -1:
-                    serial_bus.write(b'L') #Write loss to arduino
+                case -1: #Write loss to arduino
+                    serial_bus.write(b'L') 
                     result = 5
                     
                 case 2: #Write to screen
-                    arduinoWriteToScreen(serial_bus, "Testing a String Even more letters to type out!")
+                    arduinoWriteToScreen(serial_bus, displayMessage)
+                    displayMessage = "" #Reset the global string
                     result = 5
+                    
+                case 3: #Display the countdown
+                    serial_bus.write("\x02")
                     
                     
 #Making sure it only runs if its run as main and not imported
